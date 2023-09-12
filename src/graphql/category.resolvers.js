43434a08f88828f7e01fe69ce7d61420e43569aa
Edu ML <edu@eml.run>
2023-09-12 10:53:49 -0600

    (Agregar mutation con validación de JWT)

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,8 +1,16 @@
+const boom = require('@hapi/boom')
 const CategoryService = require('./../services/category.service');
 
 const service = new CategoryService();
 
-const addCategory = (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+    // { session: false } porque jwt no retiene una sesión
+    const { user } = await context.authenticate('jwt', { session: false })
+
+    if (!user) {
+        throw boom.unauthorized('jwt is not valid')
+    }
+
     return service.create(dto)
 }
 
